03c06c7535a0da25c977912cf6c78ec219135270
Raven <druidaedoug@gmail.com>
2022-12-06 22:55:07 +0300

    test commit

--- RelOKation/countries/admin.py
+++ RelOKation/countries/admin.py
@@ -1,8 +1,15 @@
-from countries.models import CountriesCard
+
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+
+from countries.models import CountriesCard
+from countries.models import UserCountriesRelation
 
 
 @admin.register(CountriesCard)
 class CountriesAdmin(ModelAdmin):
+    pass
+
+@ admin.register(UserCountriesRelation)
+class UserCountriesRelationAdmin(ModelAdmin):
     pass
--- RelOKation/countries/models.py
+++ RelOKation/countries/models.py
@@ -10,8 +10,19 @@ class CountriesCard(models.Model):
     calling_code = models.CharField(max_length=255)
     internet_tld = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_countries')
+    reader = models.ManyToManyField(User, through='UserCountriesRelation', related_name='reader_countries')
 
     def __str__(self):
         return f'{self.country_name}'
+
+
+class UserCountriesRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    countries = models.ForeignKey(CountriesCard, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    is_bookmarks = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'{self.user.username}: {self.countries}'
     
